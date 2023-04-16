a00559495e5181334f3d93349f010334b6ceb16d
Wagner Reis Rodrigues <rodrigueswagner09@gmail.com>
2023-04-16 12:34:11 -0300

    Configs ts/ Creating controller, typing data input and improving ESLINT

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,13 +1,17 @@
 import { Router } from 'express';
 import { StatusCodes } from 'http-status-codes';
 
+import { CidadesController } from './../controllers/cidades';
+
+
+
 const router = Router();
 
 router.get('/', (_, res) => {
   return res.send('Hello, DEV!');
 });
 
-router.post('/teste', (req, res) => {
+router.post('/cidades', (req, res) => {
   console.log(req.body);
 
   return res.status(StatusCodes.UNAUTHORIZED).json(req.body);
