9fa88827ee5bd15c6b7ff1be4abab097368c4123
Wagner Reis Rodrigues <rodrigueswagner09@gmail.com>
2023-04-18 19:15:45 -0300

    Yup translation and multiple error validation

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -4,21 +4,30 @@ import * as yup from 'yup';
 
 interface ICidade {
   nome: string;
+  estado: string;
 }
 
 const bodyValidation: yup.Schema<ICidade> = yup.object().shape({
   nome: yup.string().required().min(3),
+  estado: yup.string().required().min(3),
 });
 
 export const create = async (req: Request<{}, {}, ICidade>,res: Response) => {
   let validatedData: ICidade | undefined = undefined;
 
   try {
-    validatedData = await bodyValidation.validate(req.body);
+    validatedData = await bodyValidation.validate(req.body, {abortEarly: false});
   } catch (error) {
     const yupError = error as yup.ValidationError;
+    const errors: Record<string, string> = {};
 
-    return res.json({
+    yupError.inner.forEach(error => {
+    if  (!error.path) return;
+    errors[error.path] = error.message;
+    });
+
+    return res.status(StatusCodes.BAD_REQUEST).json({
+
       erros: {
         default: yupError.message,
       }
